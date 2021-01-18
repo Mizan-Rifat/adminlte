bada9e0845be2d447ff6b22b2e7b4640ef41a20a
Mizan <mizanrifat001@gmail.com>
2021-01-18 22:02:44 +0600

    components_updated

---
+++ resources/views/components/SingleImageGallery.blade.php
@@ -0,0 +1,21 @@
+<label>{{ $label }}</label>
+
+<div class="position-relative mb-2" id="img-box">
+
+    <div class="position-relative d-inline-block">
+        <img src="{{ asset($image) }}" class="img-fluid" style='max-height:200px;'>
+    </div>
+</div>
+
+
+<style>
+
+</style>
+
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+       
+        
+    });
+</script>
