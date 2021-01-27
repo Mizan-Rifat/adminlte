9221af24a988ee08bfaf82923013f6b7773295b5
Mizan <mizanrifat001@gmail.com>
2021-01-27 14:06:10 +0600

    product/events_created

---
+++ app/Listeners/ProductDeletedListener.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ProductDeleted;
+use App\Models\Product;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Storage;
+
+class ProductDeletedListener
+{
+    
+    public function __construct()
+    {
+        
+    }
+
+    
+    public function handle(ProductDeleted $event)
+    {
+
+        $products = $event->product instanceof Product ? collect([$event->product]) : $event->product;
+
+        $products->map(function($product) use($event) {
+            Storage::delete($product->image);
+
+            $product->ingredients()->sync([]);
+        });
+        
+    }
+}
